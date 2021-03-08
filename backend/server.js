efb9af0e0b02fb902908a7b59056f8d945aaae90
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-03-08 07:20:27 +0000

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const chalk = require('chalk');
+const connectDB = require('./config/db');
 
 dotenv.config({
     path: './config/config.env'
 });
+
+connectDB(); 
 
 //Get Transactions
 const transactions = require('./routes/transactions');
@@ -17,5 +20,5 @@ app.use('/api/v1/transactions', transactions)
 const PORT = process.env.PORT || 5000;
  
 app.listen(PORT, () => {
-    console.log(chalk.blue(`-- Ser Env: ${process.env.NODE_ENV}  -- Ser Port: ${process.env.PORT}`));
+    console.log(chalk.blue(`-- Ser Env: ${process.env.NODE_ENV}  -- Set Port: ${process.env.PORT}`));
 });
